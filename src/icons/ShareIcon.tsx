1ab6897130fee619639e31ff23442744610800b2
dzlek <6894255@gmail.com>
2025-07-06 22:30:58 +0300

    feat: add icons

---
+++ src/icons/ShareIcon.tsx
@@ -0,0 +1,20 @@
+import { SVGProps } from 'react'
+
+const ShareIcon = (props: SVGProps<SVGSVGElement>) => (
+  <svg
+    width="14"
+    height="12"
+    viewBox="0 0 14 12"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path
+      opacity="1"
+      d="M8.55556 0L14 5.6L8.55556 11.2V7.92C4.66667 7.92 1.94444 9.2 0 12C0.777778 8 3.11111 4 8.55556 3.2V0Z"
+      fill="currentColor"
+    />
+  </svg>
+)
+
+export { ShareIcon }
