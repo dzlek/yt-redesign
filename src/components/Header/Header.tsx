5d8a40d61aab9c338fb424218233d6c3dccfbd0e
dzlek <6894255@gmail.com>
2025-07-07 16:57:02 +0300

    add header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,24 @@
-import s from './header.module.scss'
+import { FC, useContext } from 'react'
+import { BurgerIcon } from '../../icons'
+import './header.scss'
+import { ThemeContext } from '../../context/ThemeContext'
+import InputComponent from '../input/InputComponent'
+import UserMenu from '../userMenu/UserMenu'
 
-export const Header = () => <p className={s.header}>Header</p>
+const Header: FC = () => {
+  const { lightTheme } = useContext(ThemeContext)
+  const logo = lightTheme ? '/logo/LogoLight.svg' : '/logo/LogoDark.svg'
+
+  return (
+    <p className="header">
+      <div className="headerItems">
+        <BurgerIcon />
+        <img className="logo" src={logo} alt="Logo" />
+        <InputComponent />
+      </div>
+      <UserMenu />
+    </p>
+  )
+}
+
+export default Header
