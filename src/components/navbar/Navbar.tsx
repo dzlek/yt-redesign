85a9253c9f250bc2c7f37bc7d284d4a30dd8a019
dzlek <6894255@gmail.com>
2025-07-08 17:22:49 +0300

    add navbar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,19 +1,46 @@
 import { FC } from 'react'
 import './navbar.scss'
-import { HomeIcon, TrendingIcon } from '../../icons'
+import {
+  ChevronIcon,
+  FavouritesIcon,
+  GamesIcon,
+  HistoryIcon,
+  HomeIcon,
+  LibraryIcon,
+  LikedVideosIcon,
+  MusicIcon,
+  SubscriptionsIcon,
+  TrendingIcon,
+  WatchLaterIcon,
+} from '../../icons'
+import { Link } from 'react-router-dom'
 
 const Navbar: FC = () => {
+  const menuItems = [
+    { icon: <HomeIcon />, text: 'Home', path: '/' },
+    { icon: <TrendingIcon />, text: 'Trending', path: '/' },
+    { icon: <SubscriptionsIcon />, text: 'Subscriptions', path: '/' },
+    { icon: <LibraryIcon />, text: 'Library', path: '/' },
+    { icon: <HistoryIcon />, text: 'History', path: '/history' },
+    { icon: <WatchLaterIcon />, text: 'Watch later', path: '/' },
+    { icon: <FavouritesIcon />, text: 'Favourites', path: '/' },
+    { icon: <LikedVideosIcon />, text: 'Liked videos', path: '/' },
+    { icon: <MusicIcon />, text: 'Music', path: '/' },
+    { icon: <GamesIcon />, text: 'Games', path: '/' },
+    { icon: <ChevronIcon />, text: 'Show more', path: '/' },
+  ]
+
   return (
     <div className="navbarWrapper">
       <ul className="navbarList">
-        <li>
-          <HomeIcon />
-          <p>Home</p>
-        </li>
-        <li>
-          <TrendingIcon />
-          <p>Trending</p>
-        </li>
+        {menuItems.map((item, index) => (
+          <li key={index}>
+            <Link to={item.path} className="navbarLink">
+              {item.icon}
+              <p>{item.text}</p>
+            </Link>
+          </li>
+        ))}
       </ul>
     </div>
   )
