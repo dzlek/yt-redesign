17c3233af54389c11fe275758c091c27aae072b1
dzlek <6894255@gmail.com>
2025-07-08 18:26:26 +0300

    feat: add sidebar

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useContext } from 'react'
 import './navbar.scss'
 import {
   ChevronIcon,
@@ -14,27 +14,74 @@ import {
   WatchLaterIcon,
 } from '../../icons'
 import { Link } from 'react-router-dom'
+import { ThemeContext } from '../../context/ThemeContext'
 
 const Navbar: FC = () => {
+  const { lightTheme } = useContext(ThemeContext)
+
   const menuItems = [
-    { icon: <HomeIcon />, text: 'Home', path: '/' },
-    { icon: <TrendingIcon />, text: 'Trending', path: '/' },
-    { icon: <SubscriptionsIcon />, text: 'Subscriptions', path: '/' },
-    { icon: <LibraryIcon />, text: 'Library', path: '/' },
-    { icon: <HistoryIcon />, text: 'History', path: '/history' },
-    { icon: <WatchLaterIcon />, text: 'Watch later', path: '/' },
-    { icon: <FavouritesIcon />, text: 'Favourites', path: '/' },
-    { icon: <LikedVideosIcon />, text: 'Liked videos', path: '/' },
-    { icon: <MusicIcon />, text: 'Music', path: '/' },
-    { icon: <GamesIcon />, text: 'Games', path: '/' },
-    { icon: <ChevronIcon />, text: 'Show more', path: '/' },
+    {
+      icon: lightTheme ? <HomeIcon /> : <HomeIcon opacity={0.8} />,
+      text: 'Home',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <TrendingIcon /> : <TrendingIcon opacity={0.8} />,
+      text: 'Trending',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <SubscriptionsIcon /> : <SubscriptionsIcon opacity={0.8} />,
+      text: 'Subscriptions',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <LibraryIcon /> : <LibraryIcon opacity={0.8} />,
+      text: 'Library',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <HistoryIcon /> : <HistoryIcon opacity={0.8} />,
+      text: 'History',
+      path: '/history',
+    },
+    {
+      icon: lightTheme ? <WatchLaterIcon /> : <WatchLaterIcon opacity={0.8} />,
+      text: 'Watch later',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <FavouritesIcon /> : <FavouritesIcon opacity={0.8} />,
+      text: 'Favourites',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <LikedVideosIcon /> : <LikedVideosIcon opacity={0.8} />,
+      text: 'Liked videos',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <MusicIcon /> : <MusicIcon opacity={0.8} />,
+      text: 'Music',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <GamesIcon /> : <GamesIcon opacity={0.8} />,
+      text: 'Games',
+      path: '/',
+    },
+    {
+      icon: lightTheme ? <ChevronIcon /> : <ChevronIcon opacity={0.8} />,
+      text: 'Show more',
+      path: '/',
+    },
   ]
 
   return (
     <div className="navbarWrapper">
       <ul className="navbarList">
         {menuItems.map((item, index) => (
-          <li key={index}>
+          <li key={'nav' + index}>
             <Link to={item.path} className="navbarLink">
               {item.icon}
               <p>{item.text}</p>
--- src/components/sidebar/Sidebar.tsx
+++ src/components/sidebar/Sidebar.tsx
@@ -1,12 +1,22 @@
 import { FC } from 'react'
 import Subscriptions from '../subscriptions/Subscriptions'
 import Navbar from '../navbar/Navbar'
+import { SettingIcon } from '../../icons'
+import { Link } from 'react-router-dom'
+import './sidebar.scss'
 
 const Sidebar: FC = () => {
   return (
-    <div className="sidebar">
+    <div className="sidebarWrapper">
       <Navbar />
       <Subscriptions />
+
+      <div className="settingsLink">
+        <Link to="/">
+          <SettingIcon />
+          <span> Settings</span>
+        </Link>
+      </div>
     </div>
   )
 }
