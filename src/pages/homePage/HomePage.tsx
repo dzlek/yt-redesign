187bc557da6c9df5c3c79ea41569a1f285dd3482
dzlek <6894255@gmail.com>
2025-07-15 14:52:21 +0300

    feat: add HomePage

---
+++ src/pages/homePage/HomePage.tsx
@@ -0,0 +1,26 @@
+import AuthorFeed from '../../components/authorFeed/AuthorFeed'
+import RecommendedFeed from '../../components/recommendedFeed/recommendedFeed'
+import dollieBlairAuthor from '../../shared/dollieBlairAuthor'
+import foodAndDrinkAuthor from '../../shared/foodAndDrinkAuthor'
+import recommendedFeed from '../../shared/recommendedFeed'
+
+import './homePage.scss'
+
+const HomePage = () => {
+  return (
+    <div className="homePageWrapper">
+      <AuthorFeed
+        author={dollieBlairAuthor}
+        slideWidth={250}
+        slideHeight={200}
+        hasSubscription={true}
+      />
+      <div className="hide">
+        <RecommendedFeed feed={recommendedFeed} slideWidth={540} slideHeight={300} />
+        <AuthorFeed author={foodAndDrinkAuthor} slideWidth={250} slideHeight={200} />
+      </div>
+    </div>
+  )
+}
+
+export default HomePage
