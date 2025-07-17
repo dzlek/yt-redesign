0366d3e9e9b6d99836ef7d8504ae78bb4fe73e7b
dzlek <6894255@gmail.com>
2025-07-17 15:20:23 +0300

    fix: tabItems map tabs

--- src/components/channelTabs/ChannelTabs.tsx
+++ src/components/channelTabs/ChannelTabs.tsx
@@ -1,37 +1,19 @@
 import * as Tabs from '@radix-ui/react-tabs'
 import { SearchIcon } from '../../icons'
 import './channelTabs.scss'
+import { tabItems } from './tabItems'
 
 const ChannelTabs = () => {
   return (
     <Tabs.Root defaultValue="home">
       <div className="tabsHeader">
         <Tabs.List className="tabsList">
-          <Tabs.Trigger value="home" className="tabTrigger black">
-            Home
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-          <Tabs.Trigger value="videos" className="tabTrigger">
-            Videos
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-          <Tabs.Trigger value="playlists" className="tabTrigger">
-            Playlists
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-          <Tabs.Trigger value="channels" className="tabTrigger">
-            Channels
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-          <Tabs.Trigger value="discussion" className="tabTrigger">
-            Discussion
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-          <Tabs.Trigger value="about" className="tabTrigger">
-            About
-            <span className="tabUnderline" />
-          </Tabs.Trigger>
-
+          {tabItems.map(({ value, label }) => (
+            <Tabs.Trigger key={value} value={value} className="tabTrigger">
+              {label}
+              <span className="tabUnderline" />
+            </Tabs.Trigger>
+          ))}
           <Tabs.Trigger value="search" className="tabTrigger">
             <SearchIcon />
             <span className="tabUnderline" />
---
+++ src/components/channelTabs/tabItems.ts
@@ -0,0 +1,8 @@
+export const tabItems = [
+  { value: 'home', label: 'Home' },
+  { value: 'videos', label: 'Videos' },
+  { value: 'playlists', label: 'Playlists' },
+  { value: 'channels', label: 'Channels' },
+  { value: 'discussion', label: 'Discussion' },
+  { value: 'about', label: 'About' },
+]
