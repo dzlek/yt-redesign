cabe4568770be6cd5969262e4f17219e3f4f73c8
dzlek <6894255@gmail.com>
2025-07-18 17:49:06 +0300

    feat: add-videoplayer page layout

--- src/pages/VideoPlayerPage/VideoPlayerPage.tsx
+++ src/pages/VideoPlayerPage/VideoPlayerPage.tsx
@@ -1,10 +1,27 @@
-import { Link } from 'react-router-dom'
+import './videoPlayerPage.scss'
+import SidebarRecommended from '../../components/SidebarRecommended/SidebarRecommended'
+import { recommendedSadebarChannels } from '../../shared/recommendedSadebarChannels'
 
 const VideoPlayerPage = () => {
+  // const author = DollieBlair
+
   return (
-    <h2>
-      VideoPlayerPage <Link to="/"></Link>
-    </h2>
+    <div className="videoPlayerWrapper">
+      <div className="mainContent">
+        <div className="videoPlayer">
+          {/* <VideoIframe /> */}
+          {/* <VideoInfo /> */}
+        </div>
+
+        <div className="sidebarRecommendedWrapper hideOnMobile">
+          <SidebarRecommended items={recommendedSadebarChannels} />
+        </div>
+      </div>
+
+      <div className="recommendedFeedWrapper hideOnMobile">
+        {/* <RecommendedFeed title="Next" feed={author.feed} slideWidth={250} slideHeight={200} /> */}
+      </div>
+    </div>
   )
 }
 
