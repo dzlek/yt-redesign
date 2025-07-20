e0913c85734f32b90f463e0f0f98721532ab38e5
dzlek <6894255@gmail.com>
2025-07-20 18:10:03 +0300

    fix: add notification counter

--- src/components/userMenu/UserMenu.tsx
+++ src/components/userMenu/UserMenu.tsx
@@ -5,8 +5,12 @@ const UserMenu = () => {
   return (
     <div className="userMenu">
       <div className="userMenuItems">
-        <CameraIcon className="icon" /> <MenuIcon className="icon" />
-        <NotificationsIcon className="icon" />
+        <CameraIcon className="icon" />
+        <MenuIcon className="icon" />
+        <div className="notificationContainer">
+          <NotificationsIcon className="icon" />
+          <span>3</span>
+        </div>
       </div>
       <img src="../avatars/user1.png" alt="User avatar" />
     </div>
