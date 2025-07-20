e490e5fb7565ac45f7569181ba00ad9be00fc267
dzlek <6894255@gmail.com>
2025-07-20 18:42:39 +0300

    feat: add desktop videoPlayerPage

--- src/pages/videoPlayerPage/VideoPlayerPage.tsx
+++ src/pages/videoPlayerPage/VideoPlayerPage.tsx
@@ -1,23 +1,71 @@
 import './videoPlayerPage.scss'
-import SidebarRecommended from '../../components/sidebarRecommended/SidebarRecommended'
-import { recommendedSadebarChannels } from '../../shared/recommendedSadebarChannels'
+
+import NextVideoFeed from '../../components/nextVideoFeed/NextVideoFeed'
+import nextVideosAuthor from '../../shared/nextVideosAuthor'
+import VideoPlayer from '../../components/videoPlayer/VideoPlayer'
+import FoodAndDrinkVideo from '../../shared/FoodAndDrinkVideo'
+import { DisLikeIcon, LikeIcon, ShareIcon } from '../../icons'
+import { MoreIcon } from '../../icons/MoreIcon'
+import * as Avatar from '@radix-ui/react-avatar'
+import { Link } from 'react-router-dom'
 
 const VideoPlayerPage = () => {
+  const video = FoodAndDrinkVideo
+
   return (
-    <div className="videoPlayerWrapper">
+    <div className="videoPlayerPageWrapper">
       <div className="mainContent">
         <div className="videoPlayer">
-          {/* <VideoIframe /> */}
-          {/* <VideoInfo /> */}
+          <VideoPlayer videoId={video.videoId} />
         </div>
 
-        <div className="sidebarRecommendedWrapper hideOnMobile">
-          <SidebarRecommended items={recommendedSadebarChannels} />
+        <div className="videoInfo">
+          <div className="videoHeader">
+            <h1 className="h1 title">{video.title}</h1>
+            <div className="videoSubTitle">
+              <span className="subtitle">{video.views}</span>
+              <div className="subtitleInteractionsButtons">
+                <button className="socialBtn subtitle">
+                  <LikeIcon /> {video.likes}
+                </button>
+                <button className="socialBtn subtitle">
+                  <DisLikeIcon /> {video.disLikes}
+                </button>
+                <button className="socialBtn subtitle">
+                  <ShareIcon /> Share
+                </button>
+                <button className="moreBtn">
+                  <MoreIcon />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="videoDescriptionAndSubscribe">
+            <div className="videoDescription">
+              <div className="videoChannel">
+                <Link to={video.authorLink}>
+                  <Avatar.Root className="avatarRoot">
+                    <Avatar.Image src={video.authorAvatar} alt={video.author} className="avatar" />
+                    <Avatar.Fallback className="avatarFallback">{video.author[0]}</Avatar.Fallback>
+                  </Avatar.Root>
+                </Link>
+                <div className="videoAuthorAndDescription">
+                  <h2 className="authorSubtitle h2">{video.author}</h2>
+                  <span className="published">Published on {video.publishedDate}</span>
+                  <span className="description ellipsis-multiline">{video.description}</span>
+                  <span className="paragraph showMore">Show more</span>
+                </div>
+              </div>
+            </div>
+
+            <button className="subscribeButton">Subscribe {video.subscribers}</button>
+          </div>
         </div>
       </div>
 
-      <div className="recommendedFeedWrapper hideOnMobile">
-        {/* <RecommendedFeed title="Next" feed={author.feed} slideWidth={250} slideHeight={200} /> */}
+      <div className="NextVideoFeedWrapper">
+        <NextVideoFeed author={nextVideosAuthor} />
       </div>
     </div>
   )
